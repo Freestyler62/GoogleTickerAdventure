1a05c333f9c4097fef6499c980a62712819cf50c
Francois <f.bouet@gmail.com>
2015-10-31 15:26:02 +0000

    Separate functions and top-level code
    It's a good practice to separate the two. It makes the code clearer and more
    re-usable.

--- shareprice.py
+++ shareprice.py
@@ -11,11 +11,7 @@ print 'Hello there. Welcome to Varghese Ltd. This is the stocks/share price trac
 
 # TODO: Use the 'requests' library instead of urllib to fetch the data
 import urllib, time, os, re, csv
- 
-tickers = ["AAPL"]
 
-# Define file name of the output record
-fname = "aapl.dat"
 
 def fetchGF(googleticker):
     url = "http://www.google.com/finance?&q="
@@ -28,16 +24,6 @@ def fetchGF(googleticker):
         q = "Nothing found for: " + googleticker
     return q
 
-# Display time corresponding to your location
-print(time.ctime())
-print
- 
-# Set local time zone to NYC
-os.environ['TZ'] = 'America/New_York'
-time.tzset()
-t = time.localtime()  # String
-print(time.ctime())
-print
 
 def combine(ticker):
     quote = fetchGF(ticker)  # Use the core-engine function
@@ -48,22 +34,40 @@ def combine(ticker):
     ticker = "NASDAQ:AAPL"
 
 
-freq = 600  # Fetch data every 600 sec (10 min)
+if __name__ == '__main__':
 
-with open(fname, 'a') as f:
-    writer = csv.writer(f, dialect="excel")
-    while(t.tm_hour <= 16):
-        if(t.tm_hour == 16):
-            while(t.tm_min < 01):
-                data = combine(ticker)
-                print(data)
-                writer.writerow(data)  # Save data in the file
-                time.sleep(freq)
+    tickers = ["AAPL"]
+
+    # Define file name of the output record
+    fname = "aapl.dat"
+
+    # Display time corresponding to your location
+    print(time.ctime())
+    print
+     
+    # Set local time zone to NYC
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    t = time.localtime()  # String
+    print(time.ctime())
+    print
+
+    freq = 2  # Fetch data every 600 sec (10 min)
+
+    with open(fname, 'a') as f:
+        writer = csv.writer(f, dialect="excel")
+        while(t.tm_hour <= 16):
+            if(t.tm_hour == 16):
+                while(t.tm_min < 01):
+                    data = combine(ticker)
+                    print(data)
+                    writer.writerow(data)  # Save data in the file
+                    time.sleep(freq)
+                else:
+                    break
             else:
-                break
-        else:
-            for ticker in tickers:
-                data = combine(ticker)
-                print(data)
-                writer.writerow(data)  # Save data in the file
-                time.sleep(freq)
+                for ticker in tickers:
+                    data = combine(ticker)
+                    print(data)
+                    writer.writerow(data)  # Save data in the file
+                    time.sleep(freq)
